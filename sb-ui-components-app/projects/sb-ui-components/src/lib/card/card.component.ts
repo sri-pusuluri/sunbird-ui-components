58a286d748051324d4d5904d403d41cb8d04d219
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2019-08-26 15:48:33 +0530

    Issue #SB-13915 task: Dynamic Card component

--- sb-ui-components-app/projects/sb-ui-components/src/lib/card/card.component.ts
+++ sb-ui-components-app/projects/sb-ui-components/src/lib/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input  } from '@angular/core';
 
 @Component({
   selector: 'sb-card',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  @Input() title: string;
+  @Input() topic: string;
+  @Input() subtopic: string;
+  @Input() img: string;
+  @Input() org: string;
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
