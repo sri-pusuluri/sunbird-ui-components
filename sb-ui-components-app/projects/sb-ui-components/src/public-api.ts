9cc78a81e73bb6ac7d7e88f82fbfc58bb12ed3b2
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2019-08-28 15:58:38 +0530

    Issue #SB-13915 task: Modal Component

--- sb-ui-components-app/projects/sb-ui-components/src/public-api.ts
+++ sb-ui-components-app/projects/sb-ui-components/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/sb-ui-components.service';
 export * from './lib/sb-ui-components.component';
 export * from './lib/sb-ui-components.module';
 export * from './lib/card/card.component';
+export * from './lib/modal/modal.component';
