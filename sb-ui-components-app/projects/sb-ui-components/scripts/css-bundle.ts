7908166bd1ed6d77998935b59f9616f13787fe17
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2019-08-28 12:02:33 +0530

    Issue #SB-13915 task: Dynamic Card component

---
+++ sb-ui-components-app/projects/sb-ui-components/scripts/css-bundle.ts
@@ -0,0 +1,29 @@
+import { relative } from 'path';
+import { Bundler } from 'scss-bundle';
+import { writeFile } from 'fs-extra';
+
+/** Bundles all SCSS files into a single file */
+console.log('Hii');
+async function bundleScss() {
+  const { found, bundledContent, imports } = await new Bundler()
+    .bundle('../src/lib/styles/sunbird/_styles.scss', ['../src/lib/styles/**/*.scss']);
+
+  if (imports) {
+    const cwd = process.cwd();
+
+    const filesNotFound = imports
+      .filter(x => !x.found)
+      .map(x => relative(cwd, x.filePath));
+
+    if (filesNotFound.length) {
+      console.error(`SCSS imports failed \n\n${filesNotFound.join('\n - ')}\n`);
+      throw new Error('One or more SCSS imports failed');
+    }
+  }
+
+  if (found) {
+    await writeFile('../dist/lib/styles/sunbird/_styles.scss', bundledContent);
+  }
+}
+
+bundleScss();
