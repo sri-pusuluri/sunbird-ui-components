1f7d9b56cf1aa511ad7f08b54eba32f6b45ec4fc
sri-pusuluri <srini@zool.in>
2019-09-03 10:09:01 +0530

    sb modal svg testing

---
+++ sb-ui-components-app/dist/sb-ui-components/lib/card/card.component.d.ts
@@ -0,0 +1,14 @@
+import { OnInit } from '@angular/core';
+export declare class CardComponent implements OnInit {
+    subject: string;
+    class: string;
+    medium: string;
+    category: string;
+    title: string;
+    topic: string;
+    subtopic: string;
+    img: string;
+    org: string;
+    constructor();
+    ngOnInit(): void;
+}
---
+++ sb-ui-components-app/dist/sb-ui-components/lib/sb-ui-components.component.d.ts
@@ -0,0 +1,5 @@
+import { OnInit } from '@angular/core';
+export declare class SbUiComponentsComponent implements OnInit {
+    constructor();
+    ngOnInit(): void;
+}
