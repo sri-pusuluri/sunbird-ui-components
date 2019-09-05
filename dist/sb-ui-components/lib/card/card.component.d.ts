5f8094f1695c8db669df039ddc0a238f76d4535a
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2019-09-05 18:13:58 +0530

    Issue #000 task: Cards update

--- dist/sb-ui-components/lib/card/card.component.d.ts
+++ dist/sb-ui-components/lib/card/card.component.d.ts
@@ -1,14 +1,6 @@
 import { OnInit } from '@angular/core';
 export declare class CardComponent implements OnInit {
-    subject: string;
-    class: string;
-    medium: string;
-    category: string;
-    title: string;
-    topic: string;
-    subtopic: string;
-    img: string;
-    org: string;
+    data: any;
     constructor();
     ngOnInit(): void;
 }
