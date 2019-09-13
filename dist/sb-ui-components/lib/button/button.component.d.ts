5ed14f8855e5ac9cda36acb1e92bb0f1f10e0c01
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2019-09-13 11:02:24 +0530

    Issue #000 task: update modal

--- dist/sb-ui-components/lib/button/button.component.d.ts
+++
@@ -1,7 +0,0 @@
-import { OnInit } from '@angular/core';
-export declare class ButtonComponent implements OnInit {
-    type: string;
-    align: string;
-    constructor();
-    ngOnInit(): void;
-}
--- dist/sb-ui-components/lib/card/card.component.d.ts
+++
@@ -1,6 +0,0 @@
-import { OnInit } from '@angular/core';
-export declare class CardComponent implements OnInit {
-    data: any;
-    constructor();
-    ngOnInit(): void;
-}
