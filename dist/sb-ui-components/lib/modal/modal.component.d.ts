cb939d649fad79c16bb3732141b8bac0a72317fc
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2019-09-13 11:01:43 +0530

    Issue #000 task: update modal

--- dist/sb-ui-components/lib/modal/modal.component.d.ts
+++ dist/sb-ui-components/lib/modal/modal.component.d.ts
@@ -1,13 +1,23 @@
-import { OnInit } from '@angular/core';
+import { OnInit, ElementRef, OnDestroy } from '@angular/core';
 /************************
  * Modal Wapper
+ * Syles do not apply to Child components like header, content and actions
+ * because the css selector gets modified for default angular encapsulation mode
+ * hence encapsulation: ViewEncapsulation.None is added
+ * https://stackblitz.com/edit/angular-6-custom-modal-dialog
  * *********************/
-export declare class ModalComponent implements OnInit {
+export declare class ModalComponent implements OnInit, OnDestroy {
+    private el;
+    private element;
+    showmodal: boolean;
+    modal: any;
     isClosable: boolean;
     size: string;
-    type: string;
-    constructor();
+    theme: string;
+    customClass: string;
+    constructor(el: ElementRef);
     ngOnInit(): void;
+    ngOnDestroy(): void;
 }
 /************************
  * Modal Header
